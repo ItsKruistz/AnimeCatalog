c5b3dfcb3d8728853f28a95df9fc675c10e4babf
ItsKruistz <lekruistz@gmail.com>
2024-09-17 08:52:57 +0700

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+ import { Button } from "$lib/components/ui/button";
+</script>
+
+<main>
+  <h1 class="text-3xl font-medium">
+    Anime Catalog
+  </h1>
+  <br>
+  <Button>Button</Button>
+</main>
